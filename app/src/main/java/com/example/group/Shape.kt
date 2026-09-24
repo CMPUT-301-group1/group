46d73d7177dde5b85961f768637220aeef065bce
DrakBoul <dboul63@gmail.com>
2026-09-23 20:01:05 -0600

    change default color of shape class

--- app/src/main/java/com/example/group/Shape.kt
+++ app/src/main/java/com/example/group/Shape.kt
@@ -3,5 +3,5 @@ package com.example.group
 abstract class Shape {
     abstract var x : Int
     abstract var y : Int
-    val color: String = "$PUT_YOUR_CHOICE_OF_COLOR"
+    val color: String = "cyan"
 }
